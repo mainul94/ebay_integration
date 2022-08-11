706ae3d5645f8c9b91722c2a7b454fdcbf400b43
frappeuser <frappeuser@erpnext1>
2022-08-11 17:49:28 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## eBay
+
+Pull Order from eBay and update order Status on Deispuch Delivrey Note.
+
+#### License
+
+MIT
---
+++ ebay_integration/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "eBay",
+			"color": "yellow",
+			"icon": "fa fa-integrations",
+			"type": "module",
+			"label": _("eBay")
+		}
+	]
