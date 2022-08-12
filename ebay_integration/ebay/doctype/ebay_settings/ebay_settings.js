bb9e377cc5fe57f9f2925779eba9f5cfb26975d7
Mainul <mainulkhan94@gmail.com>
2022-08-12 16:52:13 +0000

    feat: Added ebay setting Doctype.

---
+++ ebay_integration/ebay/doctype/ebay_settings/ebay_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Mainul Islam and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('eBay Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ebay_integration/ebay/doctype/ebay_settings/test_ebay_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Mainul Islam and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TesteBaySettings(unittest.TestCase):
+	pass
